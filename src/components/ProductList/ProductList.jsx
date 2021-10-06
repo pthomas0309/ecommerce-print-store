7e8bb5aae3bf47161bcc753bc8f53c14a908f20d
pthomas0309 <preston.thomas355@gmail.com>
2021-10-05 23:48:34 -0500

    create ProductList function component

---
+++ src/components/ProductList/ProductList.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+/* ProductList is a function component displaying
+a grid of divs, each with information an a product.
+This list can be sorted under certain parameters and 
+what is rendered is dependent on the array that's stored 
+in redux. */
+export default function ProductList() {
+
+    return (
+        <div>
+            This is the product list.
+        </div>
+    )
+}
